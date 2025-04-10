53ad67a057a11592209590becba006e153263e5a
Rogelio Gudiño de León <rogeliogudinodeleon@gmail.com>
2025-04-09 23:30:44 -0600

    Se vuelve a crear carpeta www

---
+++ android/app/src/main/java/com/salvador/boxApp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.salvador.boxApp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.salvador.boxApp',
   appName: 'box_app',
   webDir: 'www'
 };
